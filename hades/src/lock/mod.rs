9f07fe3808f7ac1fc4ad7a17b5738eb86a6c1328
traxys <quentin+dev@familleboyer.net>
2024-05-23 18:10:54 +0200

    hades: Correctly format

--- hades/src/lock/mod.rs
+++ hades/src/lock/mod.rs
@@ -1,9 +1,9 @@
 mod spin;
+mod spin_lazy;
 mod spin_once;
 mod spin_once_lock;
-mod spin_lazy;
 
 pub use spin::{SpinLock, SpinLockGuard};
+pub use spin_lazy::SpinLazy;
 pub use spin_once::SpinOnce;
 pub use spin_once_lock::SpinOnceLock;
-pub use spin_lazy::SpinLazy;
