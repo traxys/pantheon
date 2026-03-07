c2a99a3a3c92fb5eaba5c2f21913eccc69973db9
Quentin Boyer <quentin+dev@familleboyer.net>
2026-03-07 17:38:06 +0100

    Update flake.lock

--- flake.nix
+++ flake.nix
@@ -5,49 +5,56 @@
   inputs.naersk.url = "github:nix-community/naersk";
   inputs.rust-overlay.url = "github:oxalica/rust-overlay";
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    naersk,
-    rust-overlay,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [(import rust-overlay)];
-      };
-      rust = pkgs.rust-bin.selectLatestNightlyWith (toolchain:
-        toolchain.default.override {
-          targets = ["riscv64gc-unknown-none-elf"];
-        });
-      naersk' = pkgs.callPackage naersk {
-        cargo = rust;
-        rustc = rust;
-      };
-    in {
-      devShell = pkgs.mkShell {
-        nativeBuildInputs =
-          (with pkgs; [
-            rust
-            just
-            python3
-            cargo-binutils
-            qemu
-            ripgrep-all
-          ])
-          ++ (with pkgs.pkgsCross.riscv64.buildPackages; [
-            gcc
-          ]);
-        RUST_PATH = "${rust}";
-        RUST_DOC_PATH = "${rust}/share/doc/rust/html/std/index.html";
-        shellHook = ''
-          export PANTHEON_DIR=$(realpath .)
-          export VVK_BUILD=$PANTHEON_DIR/build
-          export PATH=$VVK_BUILD/release/native/vishvakarma:$PATH
-        '';
-      };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      naersk,
+      rust-overlay,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+        rust = pkgs.rust-bin.selectLatestNightlyWith (
+          toolchain:
+          toolchain.default.override {
+            targets = [ "riscv64gc-unknown-none-elf" ];
+          }
+        );
+        naersk' = pkgs.callPackage naersk {
+          cargo = rust;
+          rustc = rust;
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs =
+            (with pkgs; [
+              rust
+              just
+              python3
+              cargo-binutils
+              qemu
+              ripgrep-all
+            ])
+            ++ (with pkgs.pkgsCross.riscv64.buildPackages; [
+              gcc
+            ]);
+          RUST_PATH = "${rust}";
+          RUST_DOC_PATH = "${rust}/share/doc/rust/html/std/index.html";
+          shellHook = ''
+            export PANTHEON_DIR=$(realpath .)
+            export VVK_BUILD=$PANTHEON_DIR/build
+            export PATH=$VVK_BUILD/release/native/vishvakarma:$PATH
+          '';
+        };
 
-      defaultPackage = naersk'.buildPackage ./.;
-    });
+        defaultPackage = naersk'.buildPackage ./.;
+      }
+    );
 }
